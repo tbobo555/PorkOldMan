6e6beef7b7aa78d5625fe9b69e9a196c596c4ae4
tbobo555 <tbobo555@gmail.com>
2017-12-03 18:55:41 +0800

    [NEW]op test

--- core/op.go
+++ core/op.go
@@ -2,14 +2,24 @@ package core
 
 import (
     "reflect"
-    "bytes"
     "encoding/json"
+    "bytes"
+    "errors"
 )
 
+// 檢查val是否存在array或slice裡面
+// 如果有則回傳 true & 陣列索引編號
+// 如果沒有則回傳 false & -1
+// 所有非法的輸入只會回傳 false & -1，含式不會丟出error
 func InArray(val interface{}, array interface{}) (exists bool, index int) {
     exists = false
     index = -1
+    if val == nil || array == nil {
+        return
+    }
     switch reflect.TypeOf(array).Kind() {
+    case reflect.Array:
+        fallthrough
     case reflect.Slice:
         s := reflect.ValueOf(array)
 
@@ -24,21 +34,38 @@ func InArray(val interface{}, array interface{}) (exists bool, index int) {
     return
 }
 
-func DecodeCommonData(s []byte) *CommonData{
-    var result *CommonData
-    s = bytes.TrimSpace(bytes.Replace(s, []byte{'\n'}, []byte{' '}, -1))
-    err := json.Unmarshal(s, result)
-    if err != nil {
-        //todo: error here
+// 將json輸入轉成CommonData
+// 如果輸入[]byte的格式並非是CommonData，則會回傳error
+// 若輸入的[]byte無法被 json.Marshal 處理，回傳error
+func DecodeCommonData(s []byte) (*CommonData, error) {
+    if s == nil {
+        return nil, CommonError("DecodeCommonData", "nil", errors.New("nil input"))
     }
-    return result
+    var result, empty CommonData
+    err := json.Unmarshal(s, &result)
+    if err != nil {
+        return nil, CommonError("DecodeCommonData", s, err)
+    }
+    // 如果結果是empty，需要檢查輸入資料的格式是否為CommonData格式
+    // 因為 json.Unmarshal 當輸入無法解讀的格式，會回傳空的CommonData
+    if result == empty {
+        resultByte, err := json.Marshal(result)
+        if err != nil {
+            return nil, CommonError("DecodeCommonData", s, err)
+        }
+        // 不相等代表輸入s的資料並非屬於CommonData格式
+        if bytes.Compare(resultByte, s) != 0{
+            return nil, CommonError("DecodeCommonData", s, errors.New("input isn't type of CommonData"))
+        }
+    }
+    return &result, nil
 }
 
-func EncodeCommonDada(data *CommonData) []byte{
+// 將CommonData做成json
+func EncodeCommonDada(data *CommonData) ([]byte, error) {
     result, err := json.Marshal(data)
     if err != nil {
-        //todo: error here
+        return nil, CommonError("DecodeCommonData", data, err)
     }
-    result = bytes.TrimSpace(bytes.Replace(result, []byte{'\n'}, []byte{' '}, -1))
-    return result
+    return result, nil
 }
---
+++ core/op_test.go
@@ -0,0 +1,215 @@
+package core
+
+import (
+    "testing"
+    "errors"
+    "encoding/json"
+    "bytes"
+)
+
+type object struct{
+    p1 string
+}
+
+func TestInArray(t *testing.T) {
+    tests := []struct{
+        val interface{}
+        array interface{}
+        exp1 bool
+        exp2 int
+    }{
+        {0, 0, false, -1}, // not array or slice as input
+        {nil, 0, false, -1},
+        {0, nil, false, -1},
+        {nil, nil, false, -1},
+        {"a", []int{1, 2, 3}, false, -1},
+        {1, []int{1, 2, 3}, true, 0}, // slice test
+        {2, [3]int{1, 2, 3}, true, 1}, // array test
+        {[]int{3}, []int{1, 2, 3}, false, -1}, // all slice input
+        {object{"a"}, []int{1, 2, 3}, false, -1}, // object slice input
+        {object{"b"}, []object{{"a"},{"b"},{"c"}}, true, 1},
+    }
+
+    for _, test := range tests {
+        result1, result2 := InArray(test.val, test.array)
+        if result1 != test.exp1 || result2 != test.exp2 {
+            t.Errorf("Test InArray failed with input %s, %s, expect (%s, %s), get (%s, %s)",
+                test.val, test.array, test.exp1, test.exp2, result1, result2)
+        }
+    }
+}
+
+type object2 struct{
+    p1 string
+    p2 int
+    p3 error
+}
+
+func TestDecodeCommonData(t *testing.T) {
+    validData1 := &CommonData{
+        ActionType: MatchingActionKey,
+        RequestPlayerId: "test-host-id-1234567",
+        HostId: "test-host-id-1234567",
+        GuestId: "test-guest-id-1234567",
+        HostX: 40.3,
+        GuestX: 365.5,
+        HostY: 123.7,
+        GuestY: 789.5,
+    }
+    validInput1, err := json.Marshal(validData1)
+    if err != nil {
+        t.Fatalf("Fatal Error Test DecodeCommonData failed with  err: %s", err.Error())
+    }
+    validData2 := &CommonData{}
+    validInput2, err := json.Marshal(validData2)
+    if err != nil {
+        t.Fatalf("Fatal Error Test DecodeCommonData failed with  err: %s", err.Error())
+    }
+    inValidData1 := struct {
+        paramA string
+        paramB int
+        paramC *object2
+    }{
+        paramA: "123",
+        paramB: 16,
+        paramC: &object2{
+            p1: "345",
+            p2: 60,
+            p3: errors.New(""),
+        },
+    }
+    invalidInput1, err := json.Marshal(inValidData1)
+    if err != nil {
+        t.Fatalf("Fatal Error Test DecodeCommonData failed with  err: %s", err.Error())
+    }
+    invalidInput2 := []byte{71, 232, 14, 232, 168, 60}
+    tests := []struct{
+        testType string
+        input []byte
+        exp1 *CommonData
+        exp2 error
+    }{
+        {
+            testType: "valid",
+            input: validInput1,
+            exp1: validData1,
+            exp2: nil,
+        },
+        {
+            testType: "valid",
+            input: validInput2,
+            exp1: validData2,
+            exp2: nil,
+        },
+        {
+            testType: "invalid",
+            input: invalidInput1,
+            exp1: nil,
+        },
+        {
+            testType: "invalid",
+            input: invalidInput2,
+            exp1: nil,
+        },
+        {
+            testType: "invalid",
+            input: nil,
+            exp1: nil,
+        },
+    }
+
+    for _, test := range tests {
+        if test.testType == "valid" {
+            result, err := DecodeCommonData(test.input)
+            if err != nil {
+                t.Errorf("Test DecodeCommonData failed with valid input got err: %s", err.Error())
+            } else if *result != *test.exp1 || err != test.exp2 {
+                t.Errorf("Test DecodeCommonData failed with input %s, expect (%s, %s), get (%s, %s)",
+                    test.input, test.exp1, test.exp2, result, err)
+            }
+        }
+        if test.testType == "invalid" {
+            result, err := DecodeCommonData(test.input)
+            if result != test.exp1 {
+                t.Errorf("Test DecodeCommonData failed, invaid input, but get not nil result: %s", *result)
+            }
+            if err == nil {
+                t.Errorf("Test DecodeCommonData failed, invaid input, but no get error")
+            }
+        }
+    }
+}
+
+func TestEncodeCommonDada(t *testing.T) {
+    validInput1 := &CommonData{
+        ActionType: MatchingActionKey,
+        RequestPlayerId: "test-host-id-1234567",
+        HostId: "test-host-id-1234567",
+        GuestId: "test-guest-id-1234567",
+        HostX: 40.3,
+        GuestX: 365.5,
+        HostY: 123.7,
+        GuestY: 789.5,
+    }
+    input1Exp, err := json.Marshal(validInput1)
+    if err != nil {
+        t.Fatalf("Fatal Error Test EncodeCommonDada failed with  err: %s", err.Error())
+    }
+
+    validInput2 := &CommonData{
+        ActionType: "",
+        RequestPlayerId: "",
+        HostId: "",
+        GuestId: "",
+        HostX: 0.0,
+        GuestX: 0.0,
+        HostY: 0.0,
+        GuestY: 0.0,
+    }
+    input2Exp, err := json.Marshal(validInput2)
+    if err != nil {
+        t.Fatalf("Fatal Error Test EncodeCommonDada failed with  err: %s", err.Error())
+    }
+
+    validInput3 := &CommonData{
+        ActionType: "+_)(*&^%$#@!~<>",
+        RequestPlayerId: "{xxx: 789, yyy: 16}",
+        HostId: "",
+        GuestId: "",
+        HostX: 0.0,
+        GuestX: 0.0,
+        HostY: 0.0,
+        GuestY: 0.0,
+    }
+    input3Exp, err := json.Marshal(validInput3)
+    if err != nil {
+        t.Fatalf("Fatal Error Test EncodeCommonDada failed with  err: %s", err.Error())
+    }
+
+    tests := []struct{
+        input *CommonData
+        exp []byte
+    }{
+        {
+            validInput1,
+            input1Exp,
+        },
+        {
+            validInput2,
+            input2Exp,
+        },
+        {
+            validInput3,
+            input3Exp,
+        },
+    }
+    for _, test := range tests {
+        result, err := EncodeCommonDada(test.input)
+        if err != nil {
+            t.Errorf("Test EncodeCommonDada failed with valid input got err: %s", err.Error())
+        } else if bytes.Compare(result, test.exp) != 0  {
+            t.Errorf("Test EncodeCommonDada failed with input %s, expect (%s, %s), get (%s, %s)",
+                test.input, test.exp, "nil", result, err)
+        }
+    }
+}
