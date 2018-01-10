9e8960129cf02d4683de7391adbc28f9483c0f0c
tbobo555 <tbobo555@gmail.com>
2018-01-10 19:50:25 +0800

    [UPDATE]remove app engine file

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"porkoldman/core/controller"
-	"github.com/gorilla/mux"
+    "net/http"
+    "log"
+    "porkoldman/core/controller"
+    "github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.PathPrefix("/game/assets/").Handler(http.StripPrefix("/game/", http.FileServer(http.Dir("./public"))))
-	// game downstairs router
-	router.HandleFunc("/game/downstairs", controller.GameServe).Methods("GET")
-	http.Handle("/", router)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("occur error here: ", err.Error())
-	}
+    router := mux.NewRouter()
+    // test example
+    router.HandleFunc("/", controller.IndexServe)
+    router.HandleFunc("/{id:[0-9]+}", controller.IndexServe).Methods("GET")
+
+    // game downstairs router
+    router.HandleFunc("/game/downstairs", controller.GameDownStairsServeGet).Methods("GET")
+    router.HandleFunc("/io/game/downstairs/{token}", controller.IoGameDownStairsServeSocket).Methods("GET"). Headers("Connection", "Upgrade").Headers("Upgrade", "websocket")
+    router.PathPrefix("/game/assets/").Handler(http.StripPrefix("/game/", http.FileServer(http.Dir("./public"))))
+    http.Handle("/", router)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal("occur error here: ", err.Error())
+    }
 }
